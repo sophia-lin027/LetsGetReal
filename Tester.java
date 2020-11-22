f2376015ebeed1b91a1cb6bda25a909ce70b85fd
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-11-22 18:46:05 -0500

    tested equals for realnum

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+  public static void main (String[] args) {
+    RealNumber r1 = new RealNumber (1.73);
+    RealNumber r2 = new RealNumber (1.73);
+    RealNumber r3 = new RealNumber (4);
+    System.out.println(r1.getValue());
+    System.out.println(r1.equals(r3));
+    System.out.println(r1.equals(r2));
+  }
+}
