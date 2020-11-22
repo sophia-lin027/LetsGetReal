83237f7e55a3310ecaefec7986bd64dc9412f7c8
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-11-22 18:59:34 -0500

    added divide

--- RealNumber.java
+++ RealNumber.java
@@ -51,7 +51,8 @@ public class RealNumber{
   *this divided by the other
   */
   public RealNumber divide(RealNumber other){
-        return null;
+    RealNumber x = new RealNumber(value / other.getValue());
+    return x;
   }
 
   /*
