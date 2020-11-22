6fa352e39b7044ca7bef967d094c62b1a620fa10
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-11-22 18:57:05 -0500

    tested multiply for realnum

--- Tester.java
+++ Tester.java
@@ -3,9 +3,12 @@ public class Tester {
     RealNumber r1 = new RealNumber (1.73);
     RealNumber r2 = new RealNumber (1.73);
     RealNumber r3 = new RealNumber (4);
+    RealNumber r4 = new RealNumber (0);
     System.out.println(r1.getValue());
     System.out.println(r1.equals(r3));
     System.out.println(r1.equals(r2));
     System.out.println(r1.add(r3));
+    System.out.println(r1.multiply(r3));
+    System.out.println(r1.multiply(r4));
   }
 }
