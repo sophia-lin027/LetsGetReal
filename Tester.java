5dbb99b5b239f5e57aa4ce8c48168010a9f6f4e1
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-11-22 20:36:42 -0500

    tested numerator + denominator + slight changes

--- Tester.java
+++ Tester.java
@@ -4,18 +4,20 @@ public class Tester {
     RealNumber r2 = new RealNumber (1.73);
     RealNumber r3 = new RealNumber (4);
     RealNumber r4 = new RealNumber (0);
-    System.out.println(r1.getValue());
-    System.out.println(r1.equals(r3));
-    System.out.println(r1.equals(r2));
-    System.out.println(r1.add(r3));
-    System.out.println(r1.multiply(r3));
-    System.out.println(r1.multiply(r4));
-    System.out.println(r1.divide(r2));
-    System.out.println(r1.divide(r3));
-    System.out.println(r1.subtract(r3));
+    System.out.println("value of r1: " + r1.getValue());
+    System.out.println("r1 = r3: " + r1.equals(r3));
+    System.out.println("r1 = r2: " + r1.equals(r2));
+    System.out.println("r1 + r3: " + r1.add(r3));
+    System.out.println("r1 * r3: " + r1.multiply(r3));
+    System.out.println("r1 * r4: " + r1.multiply(r4));
+    System.out.println("r1 / r2: " + r1.divide(r2));
+    System.out.println("r1 / r3: " + r1.divide(r3));
+    System.out.println("r1 - r3: " + r1.subtract(r3));
     System.out.println();
 
     RationalNumber ra1 = new RationalNumber(3, 5);
-    System.out.println(ra1.getValue());
+    System.out.println("value of ra1: " + ra1.getValue());
+    System.out.println("numerator of ra1: " + ra1.getNumerator());
+    System.out.println("denominator of ra1: " + ra1.getDenominator());
   }
 }
