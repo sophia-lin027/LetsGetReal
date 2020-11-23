f7713ed4d88988d3eff7d57a4525d325fe35d55d
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-11-22 19:01:48 -0500

    added subtract

--- RealNumber.java
+++ RealNumber.java
@@ -60,6 +60,7 @@ public class RealNumber{
   *this minus the other
   */
   public RealNumber subtract(RealNumber other){
-    return null;
+    RealNumber x = new RealNumber(value - other.getValue());
+    return x;
   }
 }
